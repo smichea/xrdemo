8040b7c3b6df4ae0001eb8956e157c7139f9a897
sebastien michea <smichea@gmail.com>
2023-05-06 09:31:36 +0200

    animate cube

--- xr3.js
+++ xr3.js
@@ -5,7 +5,7 @@ const renderer = new THREE.WebGLRenderer();
 
 
 const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 500 );
-camera.position.set( 0, 0, 100 );
+camera.position.set( 0, 0, 10 );
 camera.lookAt( 0, 0, 0 );
 
 const scene = new THREE.Scene();
@@ -14,9 +14,9 @@ const scene = new THREE.Scene();
 const material = new THREE.LineBasicMaterial( { color: 0x0000ff } );
 
 const points = [];
-points.push( new THREE.Vector3( - 10, 0, 0 ) );
-points.push( new THREE.Vector3( 0, 10, 0 ) );
-points.push( new THREE.Vector3( 10, 0, 0 ) );
+points.push( new THREE.Vector3( - 2, 0, 0 ) );
+points.push( new THREE.Vector3( 0, 2, 0 ) );
+points.push( new THREE.Vector3( 2, 0, 0 ) );
 
 const cube = new THREE.Mesh( new THREE.BoxGeometry( 1, 1, 1 ),  new THREE.MeshBasicMaterial( { color: 0x00ff00 } ) );
 scene.add( cube );
@@ -34,5 +34,8 @@ renderer.xr.enabled = true;
 renderer.setAnimationLoop( function () {
 
 	renderer.render( scene, camera );
+
+	cube.rotation.x += 0.01;
+	cube.rotation.y += 0.01;
 
 } );
